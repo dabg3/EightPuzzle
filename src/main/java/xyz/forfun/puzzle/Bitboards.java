cf0cb99e74e2423701aef565312ddd8b3a163546
Davide Bigotti <dbigotti@omniagroup.it>
2022-11-29 23:03:18 +0100

    implement expandable bitboard generation

---
+++ src/main/java/xyz/forfun/puzzle/Bitboards.java
@@ -0,0 +1,53 @@
+package xyz.forfun.puzzle;
+
+import java.util.BitSet;
+import java.util.function.Function;
+import static xyz.forfun.puzzle.Options.COLUMNS;
+import static xyz.forfun.puzzle.Options.ROWS;
+
+
+public class Bitboards {
+
+    static final BitSet TOP_ROW = evaluate(Bitboards::initTopRow);
+    static final BitSet BOTTOM_ROW = evaluate(Bitboards::initBottomRow);
+    static final BitSet LEFT_COLUMN = evaluate(Bitboards::initLeftColumn);
+    static final BitSet RIGHT_COLUMN = evaluate(Bitboards::initRightColumn);
+
+    private static BitSet initTopRow(BitSet b) {
+        for (int i = 0; i < ROWS; i++) {
+            b.set(i);
+        }
+        return b;
+    }
+
+    private static BitSet initBottomRow(BitSet b) {
+        int bottomRowStartIndex = ROWS * (COLUMNS - 1) - 1;
+        for (int i = bottomRowStartIndex; i < b.size(); i++) {
+            b.set(i);
+        }
+        return b;
+    }
+
+    private static BitSet initLeftColumn(BitSet b) {
+        for (int i = 0; i < b.size(); i += ROWS) {
+            b.set(i);
+        }
+        return b;
+    }
+
+    private static BitSet initRightColumn(BitSet b) {
+        for (int i = ROWS - 1; i < b.size(); i += ROWS) {
+            b.set(i);
+        }
+        return b;
+    }
+
+    private static BitSet evaluate(Function<BitSet, BitSet> fun) {
+        BitSet bitboard = new BitSet(ROWS * COLUMNS);
+        bitboard = fun.apply(bitboard);
+        return bitboard;
+    }
+
+    private Bitboards() {
+    }
+}
