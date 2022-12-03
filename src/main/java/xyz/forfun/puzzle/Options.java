dd3bde54fa7ba6b280cb156744b4f3b9a41fc76c
Davide Bigotti <bigotti.davide@hotmail.com>
2022-12-03 18:50:47 +0100

    set 15x15 board as default

--- src/main/java/xyz/forfun/puzzle/Options.java
+++ src/main/java/xyz/forfun/puzzle/Options.java
@@ -2,8 +2,8 @@ package xyz.forfun.puzzle;
 
 public class Options {
 
-    public static final int ROWS = 3;
-    public static final int COLUMNS = 3;
+    public static final int ROWS = 15;
+    public static final int COLUMNS = 15;
     public static final int HOLE_VALUE = ROWS * COLUMNS;
     public static final int RESTART_VALUE = 0;
 
